da6466cf4095ada5906f5c7634d36269ef566ae0
webroyit <xytank@gmail.com>
2023-10-23 11:02:12 -0400

    Add ScrollControls

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
+import { ScrollControls, Scroll } from "@react-three/drei";
 
 function App() {
   return (
     <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
       <color attach="background" args={["#ececec"]} />
-      <Experience />
+      <ScrollControls pages={4} damping={0.1}>
+        <Experience />
+        <Scroll html>
+          <h1 className="text-3xl">
+            Welcome
+          </h1>
+        </Scroll>
+      </ScrollControls>
     </Canvas>
   );
 }
