7be07fe629fb5d56b4532b11162595a2a9415d83
webroyit <xytank@gmail.com>
2023-10-24 12:13:53 -0400

    Add Skills Section

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -13,14 +13,9 @@ export const Interface = () => {
     return (
         <div className="flex flex-col items-center w-screen">
             <AboutSection />
-            <Section>
-                <h1>Skills</h1>
-            </Section>
+            <SkillsSection />
             <Section>
                 <h1>Projects</h1>
-            </Section>
-            <Section>
-                <h1>About</h1>
             </Section>
             <Section>
                 <h1>Contact</h1>
@@ -45,6 +40,83 @@ const AboutSection = () => {
             <button className="bg-indigo-600 text-white py-4 px-8 rounded-lg font-bold text-lg mt-16">
                 Contact Me
             </button>
+        </Section>
+    );
+};
+
+const skills = [
+    {
+        title: "HTML",
+        level: 100
+    },
+    {
+        title: "CSS",
+        level: 95
+    },
+    {
+        title: "Javascript",
+        level: 90
+    },
+    {
+        title: "React",
+        level: 80
+    },
+    {
+        title: "Typescript",
+        level: 60
+    }
+];
+
+const languages = [
+    {
+        title: "English",
+        level: 100
+    },
+    {
+        title: "Something 1",
+        level: 50
+    },
+    {
+        title: "Something 2",
+        level: 75
+    },
+];
+
+const SkillsSection = () => {
+    return (
+        <Section>
+            <div>
+                <h2 className="text-5xl font-bold">Skills</h2>
+                <div className="mt-8 space-y-4">
+                    {skills.map((skill, index) => (
+                        <div className="w-64" key={index}>
+                            <h3 className="text-xl font-bold text-gray-800">{skill.title}</h3>
+                            <div className="h-2 w-full bg-gray-200 rounded-full">
+                                <div
+                                    className="h-2 w-full bg-indigo-500 rounded-full"
+                                    style={{ width: `${skill.level}%` }}
+                                />
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                <div>
+                    <h2 className="text-5xl font-bold mt-10">Languages</h2>
+                    <div className="mt-8 space-y-4">
+                        {languages.map((lng, index) => (
+                            <div className="w-64" key={index}>
+                                <h3 className="text-xl font-bold text-gray-800">{lng.title}</h3>
+                                <div className="h-2 w-full bg-gray-200 rounded-full">
+                                    <div
+                                        className="h-2 w-full bg-indigo-500 rounded-full"
+                                        style={{ width: `${lng.level}%` }}
+                                    />
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
         </Section>
     );
 };
