1e675611ac2196fd249c1a44baa79eb703d19540
webroyit <xytank@gmail.com>
2023-10-24 11:10:02 -0400

    Add About Section

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -10,21 +10,41 @@ const Section = (props) => {
 }
 
 export const Interface = () => {
-    return <div className="flex flex-col items-center w-screen">
+    return (
+        <div className="flex flex-col items-center w-screen">
+            <AboutSection />
+            <Section>
+                <h1>Skills</h1>
+            </Section>
+            <Section>
+                <h1>Projects</h1>
+            </Section>
+            <Section>
+                <h1>About</h1>
+            </Section>
+            <Section>
+                <h1>Contact</h1>
+            </Section>
+        </div>
+    );
+};
+
+const AboutSection = () => {
+    return (
         <Section>
-            <h1>About</h1>
+            <h1 className="text-6xl font-extrabold leading-snug">
+                Hi, I'm
+                <br />
+                <span className="bg-white px-1 italic">Webroyit</span>
+            </h1>
+            <p className="text-lg text-gray-600 mt-4">
+                I make cool website
+                <br />
+                and more cool website
+            </p>
+            <button className="bg-indigo-600 text-white py-4 px-8 rounded-lg font-bold text-lg mt-16">
+                Contact Me
+            </button>
         </Section>
-        <Section>
-            <h1>Skills</h1>
-        </Section>
-        <Section>
-            <h1>Projects</h1>
-        </Section>
-        <Section>
-            <h1>About</h1>
-        </Section>
-        <Section>
-            <h1>Contact</h1>
-        </Section>
-    </div>
-}
+    );
+};
