08a07c4e1bbbc92eccd8ebaa459968c7d8fce19b
webroyit <xytank@gmail.com>
2023-10-27 12:01:48 -0400

    Add Contact Section

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -17,9 +17,7 @@ export const Interface = () => {
             <Section>
                 <h1>Projects</h1>
             </Section>
-            <Section>
-                <h1>Contact</h1>
-            </Section>
+            <ContactSection />
         </div>
     );
 };
@@ -116,6 +114,48 @@ const SkillsSection = () => {
                         ))}
                     </div>
                 </div>
+            </div>
+        </Section>
+    );
+};
+
+const ContactSection = () => {
+    return (
+        <Section>
+            <h1 className="text-5xl font-bold">Contact Me</h1>
+            <div className="mt-8 p-8 rounded-md bg-white w-96 max-w-full">
+                <form>
+                    <label for="name" className="font-medium text-gray-900 block mb-1">
+                        Name
+                    </label>
+                    <input
+                        type="text"
+                        name="name"
+                        id="name"
+                        className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3" />
+                    <label for="email" className="font-medium text-gray-900 block mb-1 mt-8">
+                        Email
+                    </label>
+                    <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3" />
+                     <label
+                        for="message"
+                        className="font-medium text-gray-900 block mb-1 mt-8"
+                    >
+                        Message
+                    </label>
+                    <textarea
+                        name="message"
+                        id="message"
+                        className="h-32 block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
+                    />
+                    <button className="bg-indigo-600 text-white py-4 px-8 rounded-lg font-bold text-lg mt-16 ">
+                        Submit
+                    </button>
+                </form>
             </div>
         </Section>
     );
