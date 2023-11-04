4bb039296ade40d01bf40d61778a0898df312037
webroyit <xytank@gmail.com>
2023-11-04 12:27:45 -0400

    Add Menu Background

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -23,6 +23,17 @@ export const Menu = (props) => {
                     }`}
                 />
             </button>
+
+            {/* Menu Background */}
+            <div
+                className={`z-10 fixed top-0 right-0 bottom-0 bg-white transition-all overflow-hidden flex flex-col ${
+                    menuOpened ? "w-80" : "w-0"
+                }`}
+            >
+                <div className="flex-1 flex items-start justify-center flex-col gap-6 p-8">
+
+                </div>
+            </div>
         </>
-    )
-}
+    );
+};
