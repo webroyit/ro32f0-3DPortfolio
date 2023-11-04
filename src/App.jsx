16dac9ffe746dd7f70b2ed4904fd281aae5da812
webroyit <xytank@gmail.com>
2023-11-04 11:58:25 -0400

    Add hamburger icon

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,27 @@ import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
 import { ScrollControls, Scroll } from "@react-three/drei";
 import { Interface } from "./components/Interface";
+import { Menu } from "./components/Menu";
 import { ScrollManager } from "./components/ScrollManager";
 
 function App() {
   const [section, setSection] = useState(0);
+  const [menuOpened, setMenuOpened] = useState(false);
 
   return (
-    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
-      <color attach="background" args={["#ececec"]} />
-      <ScrollControls pages={4} damping={0.1}>
-        <ScrollManager section={section} onSectionChange={setSection} />
-        <Experience />
-        <Scroll html>
-          <Interface />
-        </Scroll>
-      </ScrollControls>
-    </Canvas>
+    <>
+      <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
+        <color attach="background" args={["#ececec"]} />
+        <ScrollControls pages={4} damping={0.1}>
+          <ScrollManager section={section} onSectionChange={setSection} />
+          <Experience />
+          <Scroll html>
+            <Interface />
+          </Scroll>
+        </ScrollControls>
+      </Canvas>
+      <Menu onSectionChange={setSection} menuOpened={menuOpened} setMenuOpened={setMenuOpened} />
+    </>
   );
 }
 
---
+++ src/components/Menu.jsx
@@ -0,0 +1,28 @@
+export const Menu = (props) => {
+    const { onSectionChange, menuOpened, setMenuOpened } = props;
+
+    return (
+        <>
+            <button
+                onClick={() => setMenuOpened(!menuOpened)}
+                className="z-20 fixed top-12 right-12 p-3 bg-indigo-600 w-11 h-11 rounded-md"
+            >
+                <div
+                    className={`bg-white h-0.5 rounded-md w-full transition-all ${
+                        menuOpened ? "rotate-45  translate-y-0.5" : ""
+                    }`}
+                />
+                <div
+                    className={`bg-white h-0.5 rounded-md w-full my-1 ${
+                        menuOpened ? "hidden" : ""
+                    }`}
+                />
+                <div
+                    className={`bg-white h-0.5 rounded-md w-full transition-all ${
+                        menuOpened ? "-rotate-45" : ""
+                    }`}
+                />
+            </button>
+        </>
+    )
+}
