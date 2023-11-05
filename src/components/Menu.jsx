0dfc8ca3ccd2a2cc8d369d078c47dabd5642cfa7
webroyit <xytank@gmail.com>
2023-11-05 11:41:24 -0500

    Add Menu Button

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -31,9 +31,25 @@ export const Menu = (props) => {
                 }`}
             >
                 <div className="flex-1 flex items-start justify-center flex-col gap-6 p-8">
-
+                    <MenuButton label="About" onClick={() => onSectionChange(0)} />
+                    <MenuButton label="Skills" onClick={() => onSectionChange(1)} />
+                    <MenuButton label="Projects" onClick={() => onSectionChange(2)} />
+                    <MenuButton label="Contact" onClick={() => onSectionChange(3)} />
                 </div>
             </div>
         </>
     );
 };
+
+const MenuButton = (props) => {
+    const { label, onClick } = props;
+
+    return (
+        <button
+            onClick={onClick}
+            className="text-2xl font-bold cursor-pointer hover:text-indigo-600 transition-colors"
+        >
+            {label}
+        </button>
+    )
+}
